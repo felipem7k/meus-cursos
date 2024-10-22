a7cfc2e6eee5966a16bcfee86db0a7e5cd229ad4
Felipe <felipems3004@gmail.com>
2024-10-22 12:20:47 -0300

    Javascript: promises e async/await

---
+++ alura/javascript/modulo-2/javascript-assincrono/scripts.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById('upload-btn');
+const uploadInput = document.getElementById('imagem-upload');
+
+uploadBtn.addEventListener("click", () => {
+    uploadInput.click();
+})
