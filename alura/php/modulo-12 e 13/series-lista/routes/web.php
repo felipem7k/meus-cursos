e1290eaf7c68000ce01225d80624b675cd4adb95
Felipe <felipems3004@gmail.com>
2024-10-25 09:52:20 -0300

    PHP / Laravel: transações, service container e autenticação

--- alura/php/modulo-12 e 13/series-lista/routes/web.php
+++ alura/php/modulo-12 e 13/series-lista/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +13,9 @@ Route::get('/', function () {
 Route::resource('/series', SeriesController::class)->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
 // Route::controller(SeriesController::class)->group(function() {
 //     Route::get('/series', [SeriesController::class, 'index'])->name('series.index');
 //     Route::get('/series/criar', [SeriesController::class, 'create'])->name('series.create');
