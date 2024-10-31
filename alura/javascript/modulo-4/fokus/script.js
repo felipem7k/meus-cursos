89fefbf0024505d86455eeb7fa0525713ef0624d
Felipe <felipems3004@gmail.com>
2024-10-31 10:51:25 -0300

    Javascript: manipulando elementos no DOM, aula 5

--- alura/javascript/modulo-4/fokus/script.js
+++ alura/javascript/modulo-4/fokus/script.js
@@ -6,7 +6,11 @@ const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const comecarBotao = document.getElementById('start-pause');
+const comecarBotaoSpan = document.querySelector('#start-pause span');
+const comecarBotaoImg = document.querySelector('#start-pause img');
 const musicaInput = document.getElementById('alternar-musica');
+const timer = document.getElementById('timer');
+
 const musica = new Audio('sons/luna-rise-part-one.mp3');
 
 const beep = new Audio('sons/beep.mp3');
@@ -14,10 +18,9 @@ const pause = new Audio('sons/pause.mp3');
 const play = new Audio('sons/play.wav');
 
 musica.loop = true;
-beep.loop = true;
 
 let intervaloId = null;
-let contagemEmSegundos = 5;
+let contagemEmSegundos = 1500;
 
 musicaInput.addEventListener('change', () => {
     if (musica.paused) {
@@ -54,31 +57,35 @@ function alterarContexto(contexto) {
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
             `
+            contagemEmSegundos = 1500;
             break;
         case "descanso-curto":
             titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
             ` 
+            contagemEmSegundos = 300;
             break;
         case "descanso-longo":
             titulo.innerHTML = `
             Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
             `
+            contagemEmSegundos = 900;
         default:
             break;
     }
+    zerar();
+    mostrarTempo();
 }
 
 const contagemRegressiva = () => {
     if (contagemEmSegundos <= 0) {
-        zerar();
         beep.play();
         alert("Tempo finalizado!");
-        beep.pause();
+        zerar();
         return;
     }
     contagemEmSegundos--;
-    console.log(contagemEmSegundos);
+    mostrarTempo();
 };
 
 comecarBotao.addEventListener('click', iniciarOuPausar);
@@ -90,10 +97,22 @@ function iniciarOuPausar() {
         return;
     }
     play.play();
-    intervaloId = setInterval(contagemRegressiva, 1000)
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    comecarBotaoSpan.textContent = "Pausar";
+    comecarBotaoImg.setAttribute('src', 'imagens/pause.png')
 }
 
 function zerar() {
     clearInterval(intervaloId);
     intervaloId = null;
+    comecarBotaoSpan.textContent = "Iniciar";
+    comecarBotaoImg.setAttribute('src', 'imagens/play_arrow.png');
 }
+
+function mostrarTempo() {
+    const tempo = new Date(contagemEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'});
+    timer.textContent = tempoFormatado
+}
+
+mostrarTempo();
