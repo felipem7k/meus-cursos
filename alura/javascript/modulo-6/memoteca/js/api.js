b397d42771f8bba2e87a7dc3b902f541440e3297
Felipe <felipems3004@gmail.com>
2024-11-05 11:07:08 -0300

    Javascript: implementando CRUD e requisições HTTP, aula 2

--- alura/javascript/modulo-6/memoteca/js/api.js
+++ alura/javascript/modulo-6/memoteca/js/api.js
@@ -1,10 +1,27 @@
+const uri = 'http://localhost:3000/pensamentos';
+
 const api = {
     async buscarPensamentos() {
         try {
-            const resposta = await fetch('http://localhost:3000/pensamentos');
+            const resposta = await fetch(uri);
             return await resposta.json();
         } catch (error) {
-            alert('Erro ao buscar pensamentos', error);
+            console.error(error)
+        }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const resposta = await fetch(uri,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return resposta.json();
+        } catch (error) {
+            console.error(error);
         }
     }
 }
--- alura/javascript/modulo-6/memoteca/js/main.js
+++ alura/javascript/modulo-6/memoteca/js/main.js
@@ -1,5 +1,28 @@
+import api from "./api.js";
 import ui from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+
+    const pensamentoForm = document.querySelector('#pensamento-form');
+    pensamentoForm.addEventListener('submit', manipularSubmissaoFormulario)
 });
+
+async function manipularSubmissaoFormulario(evento) {
+    evento.preventDefault();
+
+    const conteudo = document.querySelector('#pensamento-conteudo').value;
+    const autoria = document.querySelector('#pensamento-autoria').value;
+    
+    try {
+        await api.salvarPensamento({
+            conteudo, autoria
+        });
+        ui.renderizarPensamentos();
+    } catch {
+        console.error;
+    } finally {
+        conteudo = '';
+        autoria = '';
+    }
+}
