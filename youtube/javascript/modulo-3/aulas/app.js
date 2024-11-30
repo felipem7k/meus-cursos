d99d5f08c0421cd5abb5da98ff72bc286445bb8f
Felipe <felipems3004@gmail.com>
2024-11-30 18:58:37 -0300

    Introdução a POO com JS, aula 1

---
+++ youtube/javascript/modulo-3/aulas/app.js
@@ -0,0 +1,16 @@
+var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+var modificado = [];
+
+for (var i = 0; i < ingredientes.length; i++) {
+    var letraInicial = ingredientes[i].charAt(0).toUpperCase();
+    var restoTexto = ingredientes[i].slice(1).toLowerCase();
+    var resultado = letraInicial + restoTexto;
+
+    modificado[i] = resultado;
+};
+
+var ordenado = modificado.sort((a, b) => {
+    return a.localeCompare(b);
+});
+
+console.log(ordenado);
