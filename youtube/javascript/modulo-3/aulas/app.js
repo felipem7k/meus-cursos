bcabbcdcb6a5fe7a3c9fac3cab20a0d2ae905dd9
Felipe <felipems3004@gmail.com>
2024-12-01 23:21:19 -0300

    Introdução a POO com JS, aula 4

--- youtube/javascript/modulo-3/aulas/app.js
+++ youtube/javascript/modulo-3/aulas/app.js
@@ -1,26 +1,8 @@
+import modificador from "./modificador.js";
+
 var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
 
-function capitalizar(ingredientes) {
-    var modificado = [];
-
-    for (var i = 0; i < ingredientes.length; i++) {
-        var letraInicial = ingredientes[i].charAt(0).toUpperCase();
-        var restoTexto = ingredientes[i].slice(1).toLowerCase();
-        var resultado = letraInicial + restoTexto;
-    
-        modificado[i] = resultado;
-    };
-
-    return modificado;
-};
-
-function ordenar(ingredientes) {
-    return ingredientes.sort((a, b) => {
-        return a.localeCompare(b);
-    });
-};
-
-var resultadoCapitalizacao = capitalizar(ingredientes);
-var resultadoOrdenacao = ordenar(resultadoCapitalizacao);
+var resultadoCapitalizacao = modificador.capitalizar(ingredientes);
+var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizacao);
 
 console.log(resultadoOrdenacao);
