fa4e2d64b154bf82a21b027b327438c035d7c560
Felipe <felipems3004@gmail.com>
2024-12-04 10:42:05 -0300

    JavaScript: de padrões a uma abordagem funcional, aula 3

--- alura/javascript/modulo-11/project/public/app/nota/service.js
+++ alura/javascript/modulo-11/project/public/app/nota/service.js
@@ -1,18 +1,31 @@
+import { pipe, partialize } from "../utils/operators.js";
 import { handleStatus } from "../utils/promise-helpers.js"
 
 const API_URL = "http://localhost:3000/notas"
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notaService = {
     async listAll() {
-        return await fetch(API_URL).then(handleStatus);
+        return await fetch(API_URL)
+        .then(handleStatus)
+        .catch(err => {
+            console.log(err);
+            return Promise.reject("Não foi possível obter as notas fiscais.");
+        });
     },
 
     async sumItems(code) {
-        return await this.listAll().then(sumItems(code));
+        const filterItems = partialize(filterItemsByCode, code);
+        const sumNotas = pipe(
+            getItemsFromNotas,
+            filterItems,
+            sumItemsValue
+        );
+
+        return await this.listAll()
+            .then(sumNotas);
     }
 }
