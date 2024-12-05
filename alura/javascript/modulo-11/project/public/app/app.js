385ffcb9de49195fe636fa3f5f9f485706f9c633
Felipe <felipems3004@gmail.com>
2024-12-05 19:45:26 -0300

    JavaScript: de padrões a uma abordagem funcional, aula 4

--- alura/javascript/modulo-11/project/public/app/app.js
+++ alura/javascript/modulo-11/project/public/app/app.js
@@ -1,11 +1,19 @@
 import './utils/array-helper.js';
 import { notaService as service } from "./nota/service.js";
+import { debounceTime, partialize, pipe, takeUntil } from './utils/operators.js';
 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
 
-document
-.querySelector("#myButton")
-.onclick = () => 
+const action = operations(() => 
     service
     .sumItems("2143")
     .then(console.log)
-    .catch(console.error);
+    .catch(console.error)
+);
+
+document
+.querySelector("#myButton")
+.onclick = action;
--- alura/javascript/modulo-11/project/public/app/utils/operators.js
+++ alura/javascript/modulo-11/project/public/app/utils/operators.js
@@ -6,3 +6,14 @@ export const compose = (...fns) => value =>
 
 export const pipe = (...fns) => value =>
     fns.reduce((previousValue, fn) => fn(previousValue), value);
+
+export const takeUntil = (times, fn) =>
+    () => times-- > 0 && fn();
+
+export const debounceTime = (ms, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, ms);
+    };
+};
