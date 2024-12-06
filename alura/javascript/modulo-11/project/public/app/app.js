03ebef433b2b109369444684d50ba83be2631c8d
Felipe <felipems3004@gmail.com>
2024-12-06 13:54:26 -0300

    JavaScript: de padrões a uma abordagem funcional, aula 5

--- alura/javascript/modulo-11/project/public/app/app.js
+++ alura/javascript/modulo-11/project/public/app/app.js
@@ -1,6 +1,7 @@
 import './utils/array-helper.js';
 import { notaService as service } from "./nota/service.js";
 import { debounceTime, partialize, pipe, takeUntil } from './utils/operators.js';
+import { delay, retry, timeoutPromise } from './utils/promise-helpers.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -8,8 +9,7 @@ const operations = pipe(
 );
 
 const action = operations(() => 
-    service
-    .sumItems("2143")
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems("2143")))
     .then(console.log)
     .catch(console.error)
 );
--- alura/javascript/modulo-11/project/public/app/utils/promise-helpers.js
+++ alura/javascript/modulo-11/project/public/app/utils/promise-helpers.js
@@ -5,3 +5,25 @@ export const log = param => {
     console.log(param);
     return param;
 };
+
+export const timeoutPromise = (ms, promise) => {
+    const timeout = new Promise((resolve, reject) =>
+        setTimeout(() =>
+            reject(`Promessa cancelada. Tempo de ${ms}ms excedido.`), 
+            ms));
+
+    return Promise.race([timeout, promise]);
+};
+
+export const delay = ms => data =>
+        new Promise((resolve, reject) => 
+            setTimeout(() => resolve(data), ms));
+
+export const retry = (retries, ms, fn) =>
+    fn().catch(err => {
+        console.log(retries);
+        return delay(ms)().then(() => 
+            retries > 1 
+                ? retry(--retries, ms, fn) 
+                : Promise.reject(err));
+    });
