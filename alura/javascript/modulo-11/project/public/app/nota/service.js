17cb9882ccc5af093e29ed32503aad091700875c
Felipe <felipems3004@gmail.com>
2024-12-09 13:49:37 -0300

    JavaScript: de padrões a uma abordagem funcional, aula 7

--- alura/javascript/modulo-11/project/public/app/nota/service.js
+++ alura/javascript/modulo-11/project/public/app/nota/service.js
@@ -1,16 +1,18 @@
+import { Maybe } from "../utils/maybe.js";
 import { pipe, partialize } from "../utils/operators.js";
 import { handleStatus } from "../utils/promise-helpers.js"
 
 const API_URL = "http://localhost:3000/notas"
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo === code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notaService = {
     async listAll() {
         return await fetch(API_URL)
         .then(handleStatus)
+        .then(notas => Maybe.of(notas))
         .catch(err => {
             console.log(err);
             return Promise.reject("Não foi possível obter as notas fiscais.");
@@ -26,6 +28,7 @@ export const notaService = {
         );
 
         return await this.listAll()
-            .then(sumNotas);
+            .then(sumNotas)
+            .then(result => result.getOrElse(0));
     }
 }
