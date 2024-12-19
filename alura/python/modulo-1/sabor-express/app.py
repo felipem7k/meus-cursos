dee0be78e7f7fe09b51e22eaee38c7cf8914d3e3
Felipe <felipems3004@gmail.com>
2024-12-19 12:27:58 -0300

    Python: crie sua primeira aplicação, aula 2

--- alura/python/modulo-1/sabor-express/app.py
+++ alura/python/modulo-1/sabor-express/app.py
@@ -1,11 +1,36 @@
-print("""
-      𝕊𝕒𝕓𝕠𝕣 𝕖𝕩𝕡𝕣𝕖𝕤𝕤
-      """)
+import os
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+def exibir_nome_do_programa():
+    print("""
+        𝕊𝕒𝕓𝕠𝕣 𝕖𝕩𝕡𝕣𝕖𝕤𝕤
+        """)
 
-opcao_escolhida = input("Escolha uma opção: ")
-print(f"Você escolheu a opção {opcao_escolhida}")
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
+
+def finalizar_app():
+    os.system('cls')
+    print("Programa finalizado.\n")
+
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
