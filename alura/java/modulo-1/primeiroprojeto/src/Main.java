676c65d8d72bdae38b19be09dec9d38bc1e1180f
Felipe <felipems3004@gmail.com>
2024-12-23 17:31:20 -0300

    Java: criando a sua primeira aplicação, aula 2

--- alura/java/modulo-1/primeiroprojeto/src/Main.java
+++ alura/java/modulo-1/primeiroprojeto/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Esse é o Screen Match");
+
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        // Media calculada pela soma de 3 notas.
+        double media = (9.8 + 6.3 + 9.3) / 3;
+        System.out.println(media);
+
+        String sinopse = """
+                Filme: Top Gun: Maverick
+                Filme de aventura com galã dos anos 80
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+
+        System.out.println(classificacao);
     }
 }
