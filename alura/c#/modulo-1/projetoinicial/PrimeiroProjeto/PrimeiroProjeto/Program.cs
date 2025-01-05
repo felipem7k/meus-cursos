0ee89a2fd5b0f2abccc86f804398858a6ecdca20
Felipe <felipems3004@gmail.com>
2025-01-05 17:16:10 -0300

    C#: criando sua primeira aplicação, aula 2

--- alura/c#/modulo-1/projetoinicial/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ alura/c#/modulo-1/projetoinicial/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,4 +1,52 @@
 ﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound\n";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+    switch (opcaoEscolhida)
+    {
+        case 1:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 2:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 3:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine($"Você digitou a opção {opcaoEscolhida}");
+            break;
+        case -1:
+            Console.WriteLine("Programa encerrado, até a próxima!");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
 
