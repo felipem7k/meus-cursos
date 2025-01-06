b6e8babfd7d181434934756bd3c86b41e2cc8877
Felipe <felipems3004@gmail.com>
2025-01-06 18:26:32 -0300

    C#: aplicando orientação a objetos, aulas 1 e 2

---
+++ alura/c#/modulo-2/ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {this.Nome} pertence ao artista {this.Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {this.Nome}");
+        Console.WriteLine($"Artista: {this.Artista}");
+        Console.WriteLine($"Duração: {this.Duracao}");
+        Console.WriteLine(this.DescricaoResumida);
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus");
+        }
+    }
+}
---
+++ alura/c#/modulo-2/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Carta Aberta";
+musica1.Artista = "Henrique e Juliano";
+musica1.Duracao = 227;
+musica1.Disponivel = false;
+
+Musica musica2 = new()
+{
+    Nome = "O casamento da Doralice",
+    Artista = "Grupo Tradição",
+    Duracao = 211,
+    Disponivel = true
+};
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
