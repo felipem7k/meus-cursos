fcb830ce807f3ffef4712b98687a88f4fb296496
Felipe <felipems3004@gmail.com>
2025-01-08 14:36:00 -0300

    C#: aplicando orientação a objetos, aula 3

---
+++ alura/c#/modulo-2/ScreenSound/ScreenSound/Album.cs
@@ -0,0 +1,22 @@
+﻿class Album
+{
+    private List<Musica> musicas = [];
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(musica => musica.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do album {Nome}: \n");
+        foreach (var musica in musicas)
+        {
+            musica.ExibirFichaTecnica();
+            Console.WriteLine("----------------");
+        }
+        Console.WriteLine($"\nPara ouvir este album inteiro você precisa de {DuracaoTotal} segundos.");
+    }
+}
--- alura/c#/modulo-2/ScreenSound/ScreenSound/Program.cs
+++ alura/c#/modulo-2/ScreenSound/ScreenSound/Program.cs
@@ -1,16 +1,18 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Carta Aberta";
-musica1.Artista = "Henrique e Juliano";
-musica1.Duracao = 227;
-musica1.Disponivel = false;
+﻿Album albumHEJ = new();
+albumHEJ.Nome = "Manifesto Musical 2";
 
+Musica musica1 = new()
+{
+    Nome = "Última Saudade",
+    Duracao = 230
+};
 Musica musica2 = new()
 {
-    Nome = "O casamento da Doralice",
-    Artista = "Grupo Tradição",
-    Duracao = 211,
-    Disponivel = true
+    Nome = "Seja Ex",
+    Duracao = 312
 };
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumHEJ.AdicionarMusica(musica1);
+albumHEJ.AdicionarMusica(musica2);
+
+albumHEJ.ExibirMusicasDoAlbum();
