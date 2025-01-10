46fc22a529bef6010d0031d29db28de838263bac
Felipe <felipems3004@gmail.com>
2025-01-10 19:22:36 -0300

    C#: consumindo API, gravando arquivos e utilizando o LINQ, aula 4

---
+++ alura/c#/modulo-4/ScreenSound/ScreenSound/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace ScreenSound.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> Musicas { get; set; }
+
+    public MusicasPreferidas(string? nome)
+    {
+        Nome = nome;
+        Musicas = [];
+    }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        Musicas.Add(musica);
+    }
+
+    public void ExibirMusicas()
+    {
+        Console.WriteLine($"Músicas favoritas de '{Nome}'");
+        foreach (var musica in Musicas)
+        {
+            Console.WriteLine($".{musica.Nome} - {musica.Artista}");
+        }
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = Musicas
+        });
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+        
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine("Arquivo json criado com sucesso!");
+    }
+}
--- alura/c#/modulo-4/ScreenSound/ScreenSound/Program.cs
+++ alura/c#/modulo-4/ScreenSound/ScreenSound/Program.cs
@@ -12,6 +12,23 @@ using (HttpClient client = new())
         LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
         LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "blues");
         LinqFilter.FiltrarMusicasPorArtista(musicas, "XXXTENTACION");
+
+        var minhasMusicasPreferidas = new MusicasPreferidas("Felipe");
+        minhasMusicasPreferidas.AdicionarMusica(musicas[2]);
+        minhasMusicasPreferidas.AdicionarMusica(musicas[654]);
+        minhasMusicasPreferidas.AdicionarMusica(musicas[430]);
+        minhasMusicasPreferidas.AdicionarMusica(musicas[1500]);
+        minhasMusicasPreferidas.AdicionarMusica(musicas[1300]);
+
+        var outrasMusicasPreferidas = new MusicasPreferidas("Lenon");
+        outrasMusicasPreferidas.AdicionarMusica(musicas[41]);
+        outrasMusicasPreferidas.AdicionarMusica(musicas[414]);
+        outrasMusicasPreferidas.AdicionarMusica(musicas[412]);
+        outrasMusicasPreferidas.AdicionarMusica(musicas[411]);
+        outrasMusicasPreferidas.AdicionarMusica(musicas[410]);
+
+        minhasMusicasPreferidas.ExibirMusicas();
+        minhasMusicasPreferidas.GerarArquivoJson();
     }
     catch (Exception ex)
     {
