c5fb8a9685010fca481692db9437556ba96d385f
Felipe <felipems3004@gmail.com>
2025-01-13 18:03:39 -0300

    Python: aplicando Orientação a Objetos, aula 4

--- alura/python/modulo-2/sabor-express/app.py
+++ alura/python/modulo-2/sabor-express/app.py
@@ -1,30 +1,15 @@
-class Restaurante:
-    restaurantes = []
-    
-    def __init__(self, nome, categoria):
-        self._nome = nome.title()
-        self.categoria = categoria.upper()
-        self._ativo = False
-        Restaurante.restaurantes.append(self)
-    
-    def __str__(self):
-        return f'{self._nome.ljust(25)} | {self.categoria.ljust(25)} | {self.ativo.ljust(25)}'
-    
-    @classmethod
-    def listar_restaurantes(cls):
-        print(f"{'Nome:'.ljust(25)} | {'Categoria: '.ljust(25)} | {'Aberto: '.ljust(25)}")
-        for restaurante in cls.restaurantes:
-            print(restaurante)
+from modelos.restaurante import Restaurante
 
-    @property
-    def ativo(self):
-        return 'sim' if self._ativo else 'não'
-    
-    def alternar_estado(self):
-        self._ativo = not self._ativo
-    
-restaurante_praca = Restaurante('Praça','Gourmet' )
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_praca.receber_avaliacao('Felipe', 10)
+restaurante_praca.receber_avaliacao('Pedro', 6)
+restaurante_praca.receber_avaliacao('Joao', 7)
 
-Restaurante.listar_restaurantes()
+restaurante_mexicano = Restaurante('Mexican Food', 'mexicana')
+restaurante_japones = Restaurante('Japa', 'japonesa')
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == "__main__":
+    main()
---
+++ alura/python/modulo-2/sabor-express/modelos/restaurante.py
@@ -0,0 +1,46 @@
+from modelos.avaliacao import Avaliacao
+
+class Restaurante:
+    restaurantes = []
+    
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacoes = []
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self._nome.ljust(25)} | {self.categoria.ljust(25)} | {self.ativo.ljust(25)} | {str(self.media_avaliacoes).ljust(25)}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome:'.ljust(25)} | {'Categoria: '.ljust(25)} | {'Aberto: '.ljust(25)} | {'Média aval.: '.ljust(25)}")
+        for restaurante in cls.restaurantes:
+            print(restaurante)
+
+    @property
+    def ativo(self):
+        return 'sim' if self._ativo else 'não'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+        
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacoes.append(avaliacao)
+        
+    @property
+    def media_avaliacoes(self):
+        quantidade_de_notas = len(self._avaliacoes)
+        
+        if quantidade_de_notas < 1:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacoes)
+        return round(soma_das_notas / quantidade_de_notas, 1)
+    
+# restaurante_praca = Restaurante('Praça','Gourmet' )
+# restaurante_praca.alternar_estado()
+# restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+# Restaurante.listar_restaurantes()
