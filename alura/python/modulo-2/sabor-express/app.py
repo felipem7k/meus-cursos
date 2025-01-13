1f6687281655221051ca943133508aef2da047a8
Felipe <felipems3004@gmail.com>
2025-01-13 17:09:10 -0300

    Python: aplicando Orientação a Objetos, aula 3

--- alura/python/modulo-2/sabor-express/app.py
+++ alura/python/modulo-2/sabor-express/app.py
@@ -2,20 +2,29 @@ class Restaurante:
     restaurantes = []
     
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
-        
-    def __str__(self):
-        return f'{self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
+    def __str__(self):
+        return f'{self._nome.ljust(25)} | {self.categoria.ljust(25)} | {self.ativo.ljust(25)}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome:'.ljust(25)} | {'Categoria: '.ljust(25)} | {'Aberto: '.ljust(25)}")
+        for restaurante in cls.restaurantes:
             print(restaurante)
 
+    @property
+    def ativo(self):
+        return 'sim' if self._ativo else 'não'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
 restaurante_praca = Restaurante('Praça','Gourmet' )
-
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
