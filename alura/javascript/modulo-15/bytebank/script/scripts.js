88a98c1e91826197a27fc488f4de3808690bfab6
Felipe <felipems3004@gmail.com>
2025-01-23 16:17:17 -0300

    Javascript: trabalhando com threads para requisições simultâneas, aula 4

--- alura/javascript/modulo-15/bytebank/script/scripts.js
+++ alura/javascript/modulo-15/bytebank/script/scripts.js
@@ -22,17 +22,6 @@ function geraHorario() {
     return horario; 
 }
 
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao("dolar", valor);
-}
-
-setInterval(conectaAPI, 10000);
-
 function adicionarDados(grafico, legenda, dados) {
     grafico.data.labels.push(legenda);
     grafico.data.datasets.forEach(dataset => {
@@ -40,3 +29,13 @@ function adicionarDados(grafico, legenda, dados) {
     });
     grafico.update();
 }
+
+let workerDolar = new Worker("./script/workers/workerDolar.js");
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+});
