003abfd1fc3350ed6ce865effae2bca691e6a1e1
Felipe <felipems3004@gmail.com>
2025-01-24 16:40:10 -0300

    TypeScript na prática: implemente um projeto completo com TypeScript e módulos, aula 1

---
+++ alura/javascript/modulo-16/projeto-base/js/bytebank.js
@@ -0,0 +1,35 @@
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+elementoSaldo.textContent = saldo.toString();
+var elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
+    }
+    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = Number(inputValor.value);
+    var data = new Date(inputData.value);
+    if (tipoTransacao === "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao === "Transferência" || tipoTransacao === "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Transação inválida!");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
