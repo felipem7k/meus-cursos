45ad6f4ebd2b7ad55b410f13887902870f993257
Felipe <felipems3004@gmail.com>
2025-01-27 03:48:06 -0300

    TypeScript na prática: implemente um projeto completo com TypeScript e módulos, aula 5

--- alura/javascript/modulo-16/projeto-base/dist/js/types/Conta.js
+++ alura/javascript/modulo-16/projeto-base/dist/js/types/Conta.js
@@ -1,5 +1,26 @@
 import { TipoTransacao } from "./TipoTransacao.js";
-let saldo = 3000;
+let saldo = JSON.parse(localStorage.getItem("saldo")) || 0;
+const transacoes = JSON.parse(localStorage.getItem("transacoes"), (key, value) => {
+    if (key === "data") {
+        return new Date(value);
+    }
+    return value;
+}) || [];
+function debitar(valor) {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que 0!");
+    }
+    if (valor > saldo) {
+        throw new Error("Saldo insuficiente!");
+    }
+    saldo -= valor;
+}
+function depositar(valor) {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que 0!");
+    }
+    saldo += valor;
+}
 const Conta = {
     getSaldo() {
         return saldo;
@@ -7,17 +28,42 @@ const Conta = {
     getDataAcesso() {
         return new Date();
     },
+    getGruposTransacoes() {
+        const gruposTransacoes = [];
+        const listaTransacoes = structuredClone(transacoes);
+        const transacoesOrdenadas = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao = "";
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao = transacao.data.toLocaleDateString("pt-br", {
+                month: "long",
+                year: "numeric"
+            });
+            if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    listaDeTransacoes: []
+                });
+            }
+            gruposTransacoes.at(-1).listaDeTransacoes.push(transacao);
+        }
+        return gruposTransacoes;
+    },
     registrarTransacao(novaTransacao) {
         if (novaTransacao.tipoTransacao === TipoTransacao.DEPOSITO) {
-            saldo += novaTransacao.valor;
+            depositar(novaTransacao.valor);
         }
         else if (novaTransacao.tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO || novaTransacao.tipoTransacao === TipoTransacao.TRANSFERENCIA) {
-            saldo -= novaTransacao.valor;
+            debitar(novaTransacao.valor);
+            novaTransacao.valor *= -1;
         }
         else {
-            alert("Transação inválida!");
-            return;
+            throw new Error("Transação inválida!");
         }
+        transacoes.push(novaTransacao);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+        localStorage.setItem("saldo", JSON.stringify(saldo));
+        console.log(this.getGruposTransacoes());
     }
 };
 export default Conta;
--- alura/javascript/modulo-16/projeto-base/src/types/Conta.ts
+++ alura/javascript/modulo-16/projeto-base/src/types/Conta.ts
@@ -1,7 +1,33 @@
+import { GrupoTransacao } from "./GrupoTransacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 import { Transacao } from "./Transacao.js";
 
-let saldo: number = 3000;
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
+    if (key === "data") {
+        return new Date(value);
+    }
+
+    return value;
+}) || [];
+
+function debitar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que 0!");
+    }
+    if (valor > saldo) {
+        throw new Error("Saldo insuficiente!");
+    }
+
+    saldo -= valor;
+}
+
+function depositar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que 0!");
+    }
+    saldo += valor;
+}
 
 const Conta = {
     getSaldo(): number {
@@ -12,15 +38,48 @@ const Conta = {
         return new Date();
     },
 
+    getGruposTransacoes(): GrupoTransacao[] {
+        const gruposTransacoes: GrupoTransacao[] = [];
+        const listaTransacoes: Transacao[] = structuredClone(transacoes);
+        const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao: string = "";
+
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao: string = transacao.data.toLocaleDateString("pt-br", {
+                month: "long",
+                year: "numeric"
+            });
+            if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    listaDeTransacoes: []
+                });
+            }
+
+            gruposTransacoes.at(-1).listaDeTransacoes.push(transacao);
+        }
+
+        return gruposTransacoes;
+    },
+
     registrarTransacao(novaTransacao: Transacao): void {
         if (novaTransacao.tipoTransacao === TipoTransacao.DEPOSITO) {
-            saldo += novaTransacao.valor;
+            depositar(novaTransacao.valor);
         } else if (novaTransacao.tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO || novaTransacao.tipoTransacao === TipoTransacao.TRANSFERENCIA) {
-            saldo -= novaTransacao.valor;
+            debitar(novaTransacao.valor);
+            novaTransacao.valor *= -1;
         } else {
-            alert("Transação inválida!");
-            return;
+            throw new Error("Transação inválida!");
         }
+
+        transacoes.push(novaTransacao);
+
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+        localStorage.setItem("saldo", JSON.stringify(saldo));
+
+        console.log(this.getGruposTransacoes());
+        
     }
 }
 
