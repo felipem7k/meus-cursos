46254e65f74cf936d02082f1c56690b7921bbee5
Felipe <felipems3004@gmail.com>
2025-01-28 18:13:22 -0300

    Typescript Part 1: Evolving Your Javascript, aula 2

--- alura/javascript/modulo-19/arquivos-iniciais/dist/js/app.js
+++ alura/javascript/modulo-19/arquivos-iniciais/dist/js/app.js
@@ -1,9 +1,3 @@
 import Negociacao from "./models/negociacao.js";
-
-const negociacao = new Negociacao(
-    new Date(),
-    30,
-    20
-);
-
+const negociacao = new Negociacao(new Date(), 30, 20);
 console.log(negociacao.volume);
