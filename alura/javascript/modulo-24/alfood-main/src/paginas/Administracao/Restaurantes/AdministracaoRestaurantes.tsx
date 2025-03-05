c204a6fa2596110c773f9a1e9010f0f683fac6ea
Felipe <felipems3004@gmail.com>
2025-03-05 16:53:28 -0300

    React: integrando seu projeto React com APIs, aula 3

--- alura/javascript/modulo-24/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alura/javascript/modulo-24/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function AdministracaoRestaurantes() {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -12,6 +13,14 @@ export default function AdministracaoRestaurantes() {
             .catch(console.error)
     }, []);
 
+    function excluir(restaurante: IRestaurante) {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restaurante.id}/`)
+            .then(() => {
+                const listaRestaurantes = restaurantes.filter(rest => rest.id !== restaurante.id);
+                setRestaurantes(listaRestaurantes);
+            });
+    }
+
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -20,6 +29,12 @@ export default function AdministracaoRestaurantes() {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -27,6 +42,16 @@ export default function AdministracaoRestaurantes() {
                     <TableRow>
                         <TableCell key={restaurante.id}>
                             {restaurante.nome}
+                        </TableCell>
+                        <TableCell key={restaurante.id}>
+                            [<Link to={`/admin/restaurantes/${restaurante.id}`}>
+                                 editar 
+                            </Link>]
+                        </TableCell>
+                        <TableCell key={restaurante.id}>
+                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                Excluir
+                            </Button>
                         </TableCell>
                     </TableRow>
                     ))}
--- alura/javascript/modulo-24/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
+++ alura/javascript/modulo-24/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
@@ -1,21 +1,43 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 export default function FormularioRestaurantes() {
 
+    const parametros = useParams();
+    
     const [nomeRestaurante, setNomeRestaurante] = useState('');
+
+    useEffect(() => {
+        if (parametros.id) {
+            axios.get(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+                .catch(console.error);
+        }
+    }, [parametros]);
 
     function aoSubmeterForm(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        axios.post("http://localhost:8000/api/v2/restaurantes/", {
-            nome: nomeRestaurante
-        })
-            .then(() => {
 
+        if (parametros.id) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+                nome: nomeRestaurante
             })
-            .catch(console.error);
+                .then(() => {
+                    alert('Atualizado com sucesso!');
+                })
+                .catch(console.error);
+        } else {
+            axios.post("http://localhost:8000/api/v2/restaurantes/", {
+                nome: nomeRestaurante
+            })
+                .then(() => {
+                    alert('Cadastrado com sucesso!');
+                })
+                .catch(console.error);
+        }
     }
 
     return (
