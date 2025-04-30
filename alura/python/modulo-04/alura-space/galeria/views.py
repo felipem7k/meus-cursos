cb9da9ff986a0d1320b0aa8893a741061390666e
Felipe <felipems3004@gmail.com>
2025-04-30 13:56:21 -0300

    Django: templates e boas práticas, aula 5

--- alura/python/modulo-04/alura-space/galeria/views.py
+++ alura/python/modulo-04/alura-space/galeria/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'galeria/index.html')
 
-def imagem(request, imagem_id):
-    return render(request, 'galeria/imagem.html', {'imagem_id': imagem_id})
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
