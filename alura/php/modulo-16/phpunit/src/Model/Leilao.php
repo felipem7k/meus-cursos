57ca99f19a9ae7895646c26cf1996e6d8999e7a6
Potter <dkdevelopment0@gmail.com>
2025-06-16 13:59:01 -0300

    PHP e TDD: testes com PHPUnit, aula 5

--- alura/php/modulo-16/phpunit/src/Model/Leilao.php
+++ alura/php/modulo-16/phpunit/src/Model/Leilao.php
@@ -17,6 +17,15 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if ($this->ehDoUltimoUsuario($lance->getUsuario())) {
+            return;
+        }
+
+        $totalLancesDoUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+        if ($totalLancesDoUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -26,5 +35,21 @@ class Leilao
     public function getLances(): array
     {
         return $this->lances;
+    }
+
+    public function ehDoUltimoUsuario(Usuario $usuario): bool
+    {
+        if (empty($this->lances)) {
+            return false;
+        }
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $ultimoLance->getUsuario() === $usuario;
+    }
+
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        return array_reduce($this->lances, function (int $total, Lance $lanceAtual) use ($usuario) {
+            return $total + ($lanceAtual->getUsuario() === $usuario ? 1 : 0);
+        }, 0);
     }
 }
---
+++ alura/php/modulo-16/phpunit/tests/Models/LeilaoTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace Alura\Leilao\Tests\Models;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoTest extends TestCase
+{
+    public function testLeilaoNaoDeveAceitarMaisDe5LancesPorUsuario()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilao = new Leilao('Carro Antigo');
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($joao, 3000));
+        $leilao->recebeLance(new Lance($maria, 4000));
+        $leilao->recebeLance(new Lance($joao, 5000));
+        $leilao->recebeLance(new Lance($maria, 6000));
+        $leilao->recebeLance(new Lance($joao, 7000));
+        $leilao->recebeLance(new Lance($maria, 8000));
+        $leilao->recebeLance(new Lance($joao, 9000));
+        $leilao->recebeLance(new Lance($maria, 10000));
+
+        $leilao->recebeLance(new Lance($joao, 11000));
+
+        static::assertCount(10, $leilao->getLances());
+        static::assertEquals(10000, $leilao->getLances()[array_key_last($leilao->getLances())]->getValor());
+    }
+    public function testLeilaoNaoDeveReceberLancesRepetidos()
+    {
+        $leilao = new Leilao('Variante');
+        $ana = new Usuario('Ana');
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
+
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(int $qtdLances, Leilao $leilao, array $valores)
+    {
+        static::assertCount($qtdLances, $leilao->getLances());
+        
+        foreach ($valores as $i => $valorEsperado) {
+            static::assertEquals($valorEsperado, $leilao->getLances()[$i]->getValor());
+        }
+    }
+
+    public function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Carro Antigo');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Carro Novo');
+        $leilaoCom1Lance->recebeLance(new Lance($joao, 6000));
+
+        return [
+            "2-lances" => [2, $leilaoCom2Lances, [1000, 2000]],
+            "1-lance" => [1, $leilaoCom1Lance, [6000]]
+        ];
+    }
+}
