e2a4ecf098e2d71b22c4d2886f70166e4307c4e0
Potter <dkdevelopment0@gmail.com>
2025-06-25 13:52:11 -0300

    Testes de integração com PHP: testando o acesso à API e ao banco de dados, aula 2

--- alura/php/modulo-16/phpunit/tests/Integration/Dao/LeilaoDaoTest.php
+++ alura/php/modulo-16/phpunit/tests/Integration/Dao/LeilaoDaoTest.php
@@ -4,14 +4,33 @@ namespace Alura\Leilao\Tests\Integration\Dao;
 
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+    private static $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$pdo = new PDO('sqlite::memory:');
+        self::$pdo->exec('CREATE TABLE leiloes (
+            id INTEGER primary key,
+            descricao TEXT,
+            finalizado BOOL,
+            dataInicio TEXT
+        );');
+    }
+    protected function setUp(): void
+    {
+        self::$pdo->beginTransaction();
+    }
+
     public function testInsersaoEBuscaDevemFuncionar()
     {
+        
         $leilao = new Leilao("Variant 0km");
-        $leilaoDao = new LeilaoDao();
+        $leilaoDao = new LeilaoDao(self::$pdo);
 
         $leilaoDao->salva($leilao);
 
@@ -20,5 +39,10 @@ class LeilaoDaoTest extends TestCase
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame("Variant 0km", $leiloes[0]->recuperarDescricao());
+    }
+
+    protected function tearDown(): void
+    {
+        self::$pdo->rollBack();
     }
 }
