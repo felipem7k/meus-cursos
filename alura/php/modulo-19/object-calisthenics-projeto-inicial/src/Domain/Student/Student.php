47ab72097c5a91efebc76208e180c19cdbe9892b
Potter <felipems3004@gmail.com>
2025-08-01 15:09:49 -0300

    Object Calisthenics: exercitando a Orientação a Objetos, aula 2

--- alura/php/modulo-19/object-calisthenics-projeto-inicial/src/Domain/Student/Student.php
+++ alura/php/modulo-19/object-calisthenics-projeto-inicial/src/Domain/Student/Student.php
@@ -42,11 +42,10 @@ class Student
 
     private function setEmail(string $email)
     {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
-            $this->email = $email;
-        } else {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
             throw new \InvalidArgumentException('Invalid e-mail address');
         }
+        $this->email = $email;
     }
 
     public function getEmail(): string
@@ -66,19 +65,15 @@ class Student
 
     public function hasAccess(): bool
     {
-        if ($this->watchedVideos->count() > 0) {
-            $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
-            /** @var DateTimeInterface $firstDate */
-            $firstDate = $this->watchedVideos->first()->value;
-            $today = new \DateTimeImmutable();
-
-            if ($firstDate->diff($today)->days >= 90) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
+        if ($this->watchedVideos->count() <= 0) {
             return true;
         }
+
+        $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
+        /** @var DateTimeInterface $firstDate */
+        $firstDate = $this->watchedVideos->first()->value;
+        $today = new \DateTimeImmutable();
+
+        return $firstDate->diff($today)->days < 90;
     }
 }
