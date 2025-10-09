850a5507a471f6491bf89e971ffe2394937cfaf2
Felipe <felipems3004@gmail.com>
2025-10-08 21:31:11 -0300

    aulas dias 07 e 08/10/25

--- ifsc/python/ex022.py
+++ ifsc/python/ex022.py
@@ -8,7 +8,7 @@ exercicio_atual = 1
 def mostrar_titulo_exercicio():
     global exercicio_atual
     quantidade_espacos = 10
-    print(f"{"-"*quantidade_espacos} Exercício #{exercicio_atual} {"-"*quantidade_espacos}")
+    print(f"\n{"-"*quantidade_espacos} Exercício #{exercicio_atual} {"-"*quantidade_espacos}")
     exercicio_atual += 1
 
 mostrar_titulo_exercicio()
@@ -72,51 +72,222 @@ print(quadrado.eh_perfeito())
 print(quadrado.diagonal())
 
 mostrar_titulo_exercicio()
+from datetime import datetime
+
 class Pessoa:
   def __init__(self, cpf: str, nome: str, idade: int):
-    self.cpf = cpf
-    self.nome = nome
-    self.idade = idade
+    self.__cpf = cpf
+    self.__nome = nome
+    self.__idade = idade
+
+  @property
+  def nome(self) -> str:
+     return self.__nome
+
+  @property
+  def idade(self) -> int:
+     return self.__idade
 
   def __str__(self) -> str:
-    return f"Nome: {self.nome} | Idade: {self.idade} | CPF: {self.cpf}"
+    return f"Nome: {self.nome} | Idade: {self.idade} | CPF: {self.__cpf}"
 
 class ContaBancaria:
     def __init__(self, titular: Pessoa):
       self.__titular = titular
       self.__saldo = 0
+      self.__historico = []
+
+    def registrar_historico(self, descricao: str):
+      self.historico.append({
+        "descricao": descricao,
+        "data": datetime.now()
+      })
 
     def depositar(self, valor: float):
         if valor <= 0:
           raise Exception("Valor de depósito precisa ser maior que 0!")
        
-        self.__saldo += valor
+        self.saldo += valor
+        self.registrar_historico("DEPOSITO REALIZADO: R${}".format(str(valor)))
 
     def sacar(self, valor: float):
         if valor <= 0:
           raise Exception("Valor de saque precisa ser maior que 0!")
         
-        if self.__saldo < valor:
+        if self.saldo < valor:
           raise Exception("Saldo insuficiente para saque.")
 
-        self.__saldo -= valor
+        self.saldo -= valor
+        self.registrar_historico("SAQUE REALIZADO: R${}".format(str(valor)))
 
-    def pegar_saldo(self) -> float:
+    @property
+    def saldo(self) -> float:
       return self.__saldo
-    
-    def pegar_titular(self) -> Pessoa:
-      return self.__titular
 
+    @saldo.setter
+    def saldo(self, valor: float):
+      if valor < 0:
+        return
+
+      self.__saldo = valor
+
+    @property
+    def titular(self) -> Pessoa:
+      return self.__titular
+    
+    @property
+    def historico(self) -> map:
+      return self.__historico
+    
+    def extrato(self) -> str:
+      lista = [f"{datetime.strftime(elemento["data"], "%d/%m/%Y %H:%M:%S")} | {elemento["descricao"]}" for elemento in self.historico]
+      return "\n".join(lista)
+    
 try:
   conta_1 = ContaBancaria(Pessoa("123456789", "Alice", 20))
-  valor_deposito = 1000.00
-  conta_1.depositar(valor_deposito)
-  print(f"Depósito de R${valor_deposito:.2f} realizado")
+  conta_1.depositar(1000.00)
 
-  valor_saque = 500.00
-  conta_1.sacar(valor_saque)
-  print(f"Saque de R$ {valor_saque:.2f} realizado")
+  conta_1.sacar(500.00)
 
-  print(f"Saldo disponível de ({conta_1.pegar_titular()}): R$ {conta_1.pegar_saldo():.2f}")
+  print(f"{"-"*10} EXTRATO {"-"*10}")
+  print(conta_1.extrato())
+
+  print("-"*25)
+
+  print(f"Saldo disponível de {conta_1.titular.nome}: R${conta_1.saldo:.2f}")
 except Exception as e:
   print(f"[ERRO]: {e}")
+
+mostrar_titulo_exercicio()
+class Dispositivo:
+  def __init__(self, temperatura: float) -> None:
+    self.__temperatura = temperatura
+
+  @property
+  def temperatura(self) -> float:
+    return self.__temperatura
+  
+  @temperatura.setter
+  def temperatura(self, valor) -> None:
+    min_temp, max_temp = 0, 100
+    if valor < min_temp:
+      valor = min_temp
+    elif valor > max_temp:
+      valor = max_temp
+    self.__temperatura = valor
+
+dispositivo = Dispositivo(30)
+dispositivo.temperatura = 10
+dispositivo.temperatura = 200
+
+print(f"Temperatura atual do dispositivo: {dispositivo.temperatura}°C")
+
+mostrar_titulo_exercicio()
+
+import hashlib
+
+class Usuario():
+  def __init__(self):
+    self.__senha = None
+
+  def definir_senha(self, senha: str) -> None:
+    hash_senha = hashlib.sha256(senha.encode()).hexdigest()
+    self.__senha = hash_senha
+
+  def autenticar(self, senha: str) -> bool:
+    hash_senha = hashlib.sha256(senha.encode()).hexdigest()
+    return hash_senha == self.__senha
+  
+usuario = Usuario()
+usuario.definir_senha("12345")
+if usuario.autenticar("321"):
+  print("Logado com sucesso!")
+else:
+  print("Usuário ou senha inválidos.")
+
+mostrar_titulo_exercicio()
+
+class Retangulo:
+  __largura = 0
+  __altura = 0
+
+  def __init__(self, largura: float, altura: float):
+    self.largura = largura
+    self.altura = altura
+
+  def __validar_comprimento(self, comprimento: float) -> float:
+    if comprimento < 0:
+      return False
+
+    return True
+
+  @property
+  def largura(self) -> float:
+    return self.__largura
+
+  @largura.setter
+  def largura(self, comprimento: float) -> None:
+    if not self.__validar_comprimento(comprimento):
+      raise ValueError("Largura precisa ser maior que 0!")
+    self.__largura = comprimento
+
+  @property
+  def altura(self) -> float:
+     return self.__altura
+  
+  @altura.setter
+  def altura(self, comprimento: float) -> None:
+    if not self.__validar_comprimento(comprimento):
+      raise ValueError("Altura precisa ser maior que 0!")
+    self.__altura = comprimento
+
+try:
+  retangulo = Retangulo(3, 6)
+  print(f"Retangulo: l={retangulo.largura}, a={retangulo.altura}")
+  retangulo.largura = -1
+  print(f"Retangulo: l={retangulo.largura}, a={retangulo.altura}")
+except ValueError as e:
+  print(f"[ERRO AO ADICIONAR VALOR]: {e}")
+
+mostrar_titulo_exercicio()
+
+class Produto:
+  __nome = ""
+  __preco = 0
+
+  def __init__(self, nome, preco):
+    self.nome = nome
+    self.preco = preco
+
+  @property
+  def nome(self) -> str:
+    return self.__nome
+  
+  @nome.setter
+  def nome(self, nome: str) -> None:
+    self.__nome = nome
+
+  @property
+  def preco(self) -> bool:
+    return self.__preco
+  
+  @preco.setter
+  def preco(self, preco: float) -> None:
+    if preco < 0:
+      raise ValueError("Preço precisa ser maior que 0!")
+    
+    self.__preco = preco
+
+  def exibir_detalhes(self):
+    return f"Nome: {self.nome} | Preço: {self.preco}"
+  
+try:
+  mouse = Produto("Mouse Logitech", 300)
+  print(mouse.exibir_detalhes())
+  mouse.nome = "Mouse Razer"
+  mouse.preco = 500
+  print(mouse.exibir_detalhes())
+  mouse.preco = -1
+  print(mouse.exibir_detalhes())
+except ValueError as e:
+  print(f"Erro ao cadastrar produto: {e}")
