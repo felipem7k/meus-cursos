f1e7312c8e287a5f9f122caedae08396aa584514
Potter <dkdevelopment0@gmail.com>
2025-12-18 20:45:56 -0300

    Laravel: modulo 1, aula 6

--- b7web/php/modulo-01/projeto-aulas/routes/web.php
+++ b7web/php/modulo-01/projeto-aulas/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [SiteController::class, 'index']);
+
+// Route::get('/', function () {
+//     return "ainda no router";
+// });
