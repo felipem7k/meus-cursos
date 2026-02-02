3ab9c76911f41a0bd7af59ac34055311af87ea3a
Potter <dkdevelopment0@gmail.com>
2026-02-01 22:59:40 -0300

    Java: modulo 1, aula 2 + exercicio

--- nelio-alves/java/modulo-01/atividade-01/curso_programacao/src/Main.java
+++ nelio-alves/java/modulo-01/atividade-01/curso_programacao/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        double x = 3.123124;
+        int y = 32;
+
+        System.out.printf("%.2f%n", x);
+        System.out.println(y);
+
         System.out.println("Hello world!");
+
+        System.out.println("RESULTADO = " + x + " metros");
+        System.out.printf("RESULTADO = %.2f metros%n", x);
+
+        String nome = "Maria";
+        int idade = 40;
+        double salario = 2000.0;
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f%n", nome, idade, salario);
+
     }
 }
